1c4c69d7cba48436a98264021972c6e8eefd2b9a
Ankit Shukla <Ankit Shukla@DESKTOP-4JQ48QB>
2022-10-29 11:41:38 +0530

    it is done

---
+++ exam/exam/src/main/java/QuesManagement/Answer2.java
@@ -0,0 +1,45 @@
+package QuesManagement;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ans")
+public class Answer2 {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int aId;
+	@Column(name="answer")
+	private String aName;
+	
+	public int getaId() {
+		return aId;
+	}
+	public void setaId(int aId) {
+		this.aId = aId;
+	}
+	public String getaName() {
+		return aName;
+	}
+	public void setaName(String aName) {
+		this.aName = aName;
+	}
+	public Answer2(int aId, String aName) {
+		super();
+		this.aId = aId;
+		this.aName = aName;
+	}
+	@Override
+	public String toString() {
+		return "Answer [aId=" + aId + ", aName=" + aName + "]";
+	}
+	public Answer2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
