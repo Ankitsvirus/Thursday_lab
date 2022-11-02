a122fa23423de38a30dd0b4da8238666bded84d9
Ankit Shukla <Ankit Shukla@DESKTOP-4JQ48QB>
2022-11-02 23:58:41 +0530

    done

---
+++ HQL/src/main/java/hql/Employee.java
@@ -0,0 +1,16 @@
+package hql;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+private String name;
+@Id
+private int id;
+private int salary;
+
+
+}
---
+++ HQL/src/main/java/hql/HqlQuery.java
@@ -0,0 +1,72 @@
+package hql;
+import java.util.List;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HqlQuery {
+
+	public static void main(String[] args) {
+		SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		Session session=factory.openSession();
+		session.beginTransaction();
+		//insertion in HQL:
+		Employee e=new Employee();
+		e.setId(1);
+		e.setName("tanuja");
+		e.setSalary(25000);
+		session.save(e);
+		
+		
+		Employee e1=new Employee();
+		e1.setId(2);
+		e1.setName("anchal");
+		e1.setSalary(20000);
+		session.save(e1);
+		session.getTransaction().commit();
+		//select all rows using HQL;
+		Query q=session.createQuery("from Employee");
+		List list=q.getResultList();
+		System.out.println(list);
+		
+//updation in HQL;
+		session.beginTransaction();
+		Query query=session.createQuery("update Employee set name=:n where id=:i");
+		query.setParameter("n", "Ani");
+		query.setParameter("i", 2);
+		int status=query.executeUpdate();
+		System.out.println(status);
+		session.getTransaction().commit();
+		
+		
+		//delete in Hql;
+		/*session.beginTransaction();
+		Query query=session.createQuery("delete from Employee where id=2");
+		int status=query.executeUpdate();
+		System.out.println("no of delete is "+status);*/
+		// aggrigation in hql;
+		session.beginTransaction();
+		Query query4=session.createQuery("select sum salary from Employee");
+		List<Double> list4=query4.getResultList();
+		System.out.println(list4);
+		
+		
+		Query query1=session.createQuery("select max salary from Employee");
+		List<Double> list1=query1.getResultList();
+		System.out.println(list1);
+		
+		
+		Query query2=session.createQuery("select min salary from Employee");
+		List<Double> list2=query2.getResultList();
+		System.out.println(list2);
+		
+		
+		
+		Query query3=session.createQuery("select avg(salary from Employee");
+		List<Double> list3=query3.getResultList();
+		System.out.println(list3);
+		session.close();
+		factory.close();
+	}
+}
