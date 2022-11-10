8f28fb9437a4f542ed5916ebe3a14b493c83cf1f
Ankit Shukla <Ankit Shukla@DESKTOP-4JQ48QB>
2022-11-11 00:05:36 +0530

    Complete

---
+++ CollegeManagementSystem/src/test/java/com/college/AppTest.java
@@ -0,0 +1,26 @@
+package com.college;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class AppTest{
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		int fees=0;
+		if(fees<=1)
+		{
+			System.out.println("fees paid");
+		}
+		else
+		{
+			System.out.println("not paid");
+		}
+	}
+
+}
